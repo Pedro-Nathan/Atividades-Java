5a8d5e7141012a4a7acd83206fe3008cfefa800e
Pedro Nathan de Souza Araújo <natansouzaaraujo732@gmail.com>
2025-04-25 21:18:55 -0300

    Update Ex1.java
    Comentando no Código .

--- ex1/src/ex1/Ex1.java
+++ ex1/src/ex1/Ex1.java
@@ -1,9 +1,15 @@
 /* Criar um programa que imprima a mensagem de saudação “Olá MUNDO!!”*/
 package ex1;
-public class Ex1 {
+public class Ex1 { // Nome da minha classe 
+    
+    // "public" Permite que o método seja acessado pela JVM de fora da classe
+    // "static" O método pode ser executado sem criar um objeto da classe
+    // "void" O método não retorna nenhum valor
+    // "main" é o nome fixo que a JVM procura para iniciar o programa
+    // "String[] args" Parâmetro padrão do método main que armazena os argumentos passados na execução do programa
     public static void main(String[] args) {
         
-        System.out.println("Olá MUNDO!!");
+        System.out.println("Olá MUNDO!!");//"System" indica o sistema "out" representa a sainda "println" imprime a mensagem e pula uma linha
         
     }
 }
